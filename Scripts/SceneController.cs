049c5e39886d2e7cdf0aa3c2eed4ae066129b505
Ricard Orpí <61988635+RicardOP-gdev@users.noreply.github.com>
2021-03-31 14:35:52 +0200

    Scripts in project

---
+++ Scripts/SceneController.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleARCore;
+using GoogleARCore.Examples.Common;
+
+
+public class SceneController : MonoBehaviour
+{
+
+    public Camera firstPersonCamera;
+    public ScoreboardController scoreboard;
+    public ScoreboardController timer;
+    public SnakeController snakeController;
+    public bool stopVisualizePlane;
+    public ARCoreSessionConfig SessionConfig;
+    public static float timerGame;
+    public static float refreshTimerGame = 5;
+    public static bool initGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        QuitOnConnectionErrors();
+        SessionConfig.PlaneFindingMode = DetectedPlaneFindingMode.Horizontal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //timer.text = timerGame.ToString("#.00");
+        // The session status must be Tracking in order to access the Frame.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+            return;
+        }
+        Debug.Log(ScoreboardController.modeGame);
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        ProcessTouches();
+        scoreboard.SetScore(snakeController.GetLength());    
+        scoreboard.SetMode(ScoreboardController.modeGame);    
+        if (initGame)
+        {
+            timerGame -= Time.deltaTime;
+            scoreboard.SetTimer(timerGame);
+            if (timerGame <= 0)
+            {
+                timerGame = 0;
+                GameOver(); 
+            }
+        }   
+    }
+
+    void QuitOnConnectionErrors()
+    {
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            StartCoroutine(CodelabUtils.ToastAndExit(
+                "Camera permission is needed to run this application.", 5));
+        }
+        else if (Session.Status.IsError())
+        {
+            // This covers a variety of errors.  See reference for details
+            // https://developers.google.com/ar/reference/unity/namespace/GoogleARCore
+            StartCoroutine(CodelabUtils.ToastAndExit(
+                "ARCore encountered a problem connecting. Please restart the app.", 5));
+        }
+    }
+
+    void ProcessTouches()
+    {
+        Touch touch;
+        if (Input.touchCount != 1 ||
+            (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter =
+            TrackableHitFlags.PlaneWithinBounds |
+            TrackableHitFlags.PlaneWithinPolygon;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            SetSelectedPlane(hit.Trackable as DetectedPlane);
+        }
+    }
+
+    void SetSelectedPlane(DetectedPlane selectedPlane)
+    {  
+        if(GameObject.FindGameObjectWithTag("food") != null)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("food"));
+        }
+        Debug.Log("Selected plane centered at " + selectedPlane.CenterPose.position);
+        scoreboard.SetSelectedPlane(selectedPlane);
+        snakeController.SetPlane(selectedPlane);
+        GetComponent<FoodController>().SetSelectedPlane(selectedPlane);
+        GetComponent<FoodController>().SpawnFoodInstance();
+        initGame = true;
+        timerGame = refreshTimerGame;
+        FindObjectOfType<SnakeController>().speed = 20;
+        HidePlanes();
+    }
+
+   /* public void StopVisualizePlanes(bool showPlane)
+    {
+        
+    } */
+
+    public void GameOver()
+    {
+        timerGame = 0;
+        initGame = false;
+        GameObject food = GameObject.FindGameObjectWithTag("food");
+        Destroy(food);
+        FindObjectOfType<SnakeController>().speed = 0;       
+    }   
+
+    public void HidePlanes()
+    {
+        foreach (GameObject plane in GameObject.FindGameObjectsWithTag("plane"))
+        {
+            Renderer r = plane.GetComponent<Renderer>();
+            DetectedPlaneVisualizer t = plane.GetComponent<DetectedPlaneVisualizer>();
+            r.enabled = false;
+            t.enabled = false;
+            SessionConfig.PlaneFindingMode = DetectedPlaneFindingMode.Disabled;
+        }
+    }
+}
---
+++ Scripts/ScoreboardController.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class ScoreboardController : MonoBehaviour
+{
+
+    public Camera firstPersonCamera;
+    private Anchor anchor;
+    private DetectedPlane detectedPlane;
+    private float yOffset;
+    private int score;
+    private float timeGame;
+    public static string modeGame;
+    private string modeGame2;
+  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            r.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        timeGame = SceneController.timerGame;
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+        if (detectedPlane == null)
+        {
+            return;
+        }
+
+        // Check for the plane being subsumed.
+        // If the plane has been subsumed switch attachment to the subsuming plane.
+        while (detectedPlane.SubsumedBy != null)
+        {
+            detectedPlane = detectedPlane.SubsumedBy;
+        }
+
+        // Make the scoreboard face the viewer.
+        transform.LookAt(firstPersonCamera.transform);
+
+        // Move the position to stay consistent with the plane.
+        transform.position = new Vector3(transform.position.x,
+                    detectedPlane.CenterPose.position.y + yOffset, transform.position.z);
+    }
+
+    public void SetSelectedPlane(DetectedPlane detectedPlane)
+    {
+        this.detectedPlane = detectedPlane;
+        CreateAnchor();
+    }
+
+    void CreateAnchor()
+    {
+        // Create the position of the anchor by raycasting a point towards
+        // the top of the screen.
+        Vector2 pos = new Vector2(Screen.width * .5f, Screen.height * .90f);
+        Ray ray = firstPersonCamera.ScreenPointToRay(pos);
+        Vector3 anchorPosition = ray.GetPoint(5f);
+
+        // Create the anchor at that point.
+        if (anchor != null)
+        {
+            DestroyObject(anchor);
+        }
+        anchor = detectedPlane.CreateAnchor(
+            new Pose(anchorPosition, Quaternion.identity));
+
+        // Attach the scoreboard to the anchor.
+        transform.position = anchorPosition;
+        transform.SetParent(anchor.transform);
+
+        // Record the y offset from the plane.
+        yOffset = transform.position.y - detectedPlane.CenterPose.position.y;
+
+        // Finally, enable the renderers.
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            r.enabled = true;
+        }
+    }
+
+    public void SetScore(int score)
+    {
+        GameObject scoreText = GameObject.FindGameObjectWithTag("score"); 
+        if (this.score != score)
+        {
+            scoreText.GetComponent<TextMesh>().text = "Score: " + score;
+            this.score = score;
+        }
+    }
+
+    public void SetTimer(float time)
+    {
+        GameObject timerText = GameObject.FindGameObjectWithTag("time");
+        if (this.timeGame != time)
+        {
+            timerText.GetComponent<TextMesh>().text = "Time: " + timeGame.ToString("F2");
+            this.timeGame = time;
+
+            if (Slithering.bodyParts.Count >= 0 && Slithering.bodyParts.Count <= 9)
+            {
+                SceneController.refreshTimerGame = 5;
+                timerText.GetComponent<TextMesh>().color = Color.green;
+            }
+
+            else if (Slithering.bodyParts.Count >= 10)
+            {
+                SceneController.refreshTimerGame = 3;
+                timerText.GetComponent<TextMesh>().color = Color.red;
+            }
+        }
+    }
+
+    public void SetMode(string mode)
+    {        
+       
+        
+        
+    }
+}
